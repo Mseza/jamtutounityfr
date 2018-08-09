33e8f140926ecdf4ef4dd58e969dfbf9b41b9bfb
Mseza <l_gottar@orange.fr>
2018-08-09 18:10:25 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  let guild = message.member.guild;
+  let jamParticipant = guild.roles.find("name", 'JamParticipant');
+
+  if(message.channel.id === '477136958188158996'){
+
+    if(message.content = '!jeparticipe'){
+        message.member.addRole(jamParticipant);
+        message.reply("vous êtes désormais inscrit à la GameJam");
+      }
+
+    }
+
+  }
+
+});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
